1689e6f0218fafcbb7a393153ebe46a514a11d66
drop-simon <simon@earnwithdrop.com>
2020-08-03 17:27:12 -0400

    sdkfjh

--- src/recipes/Chess/index.ts
+++ src/recipes/Chess/index.ts
@@ -1,7 +1,21 @@
 import Chess from './Chess'
+import ChessTeam from './ChessTeam'
 
 const chess = new Chess()
 
-let state = chess.start()
+const renderBoard = () => {
+  const pathfinders = chess.board.get.pathfinders()
 
-// stuff
+  return chess.board.graph.reduce((acc, row, y) => {
+    const tiles = row.map(({ coords }, x) => {
+      const unit = pathfinders.find(p => p.coordinates.hash === coords.hash)
+        ?.unit
+
+      let char = (x + y) % 2 ? '░░' : '▓▓'
+      if (unit)
+        char = char.slice(0, 1) + (unit.team as ChessTeam).type.slice(0, 1)
+      return char
+    })
+    return acc + tiles.join('') + `\n`
+  }, '')
+}
