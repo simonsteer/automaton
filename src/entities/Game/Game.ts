f379154879063768951dfc8af42bfd1f7c1928b3
drop-simon <simon@earnwithdrop.com>
2020-08-03 01:43:33 -0400

    ting

--- src/entities/Game/Game.ts
+++ src/entities/Game/Game.ts
@@ -22,4 +22,19 @@ export default class Game {
       tile: new Tile(defaultTerrain),
     }
   }
+
+  clear = {
+    units: () => this.entities.unit.clear(),
+    grids: () => this.entities.grid.clear(),
+    terrain: () => this.entities.terrain.clear(),
+    teams: () => this.entities.team.clear(),
+    weapons: () => this.entities.weapon.clear(),
+    entities: () => {
+      this.clear.grids()
+      this.clear.terrain()
+      this.clear.teams()
+      this.clear.units()
+      this.clear.weapons()
+    },
+  }
 }
