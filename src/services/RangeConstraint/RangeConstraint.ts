2728518949a1c214ff22dd70a5dada402bec5741
drop-simon <simon@earnwithdrop.com>
2020-08-04 14:01:39 -0400

    refactor RangeConstraint to take multiple configs; prep work for fixing some pathfinder stuff

--- src/services/RangeConstraint/RangeConstraint.ts
+++ src/services/RangeConstraint/RangeConstraint.ts
@@ -3,56 +3,42 @@ import { RangeConstraintConfig } from './types'
 import Coords from '../Coords'
 
 export default class RangeConstraint {
-  constraint: RangeConstraintConfig
-  offsets: {
-    x: Set<number>
-    y: Set<number>
+  constraints: RangeConstraintConfig[]
+  constructor(...constraints: RangeConstraintConfig[]) {
+    this.constraints = constraints
   }
 
-  constructor(constraint: RangeConstraintConfig) {
-    this.constraint = constraint
-    this.offsets = this.buildRangeSets()
-  }
-  /**
-   * determines whether the distance between two coordinates matches the constraint
-   */
-  applies = (coordsA: Coords, coordsB: Coords) => {
-    const deltas = coordsA.deltas(coordsB)
-    return Object.values(this.validations).every(validate => validate(deltas))
-  }
   /**
    * get coordinates considered adjacent to the coordinates passed in
    */
   adjacent(coordsA: Coords) {
     const adjacentCoords: Coords[] = []
-    for (const xOffset of this.offsets.x) {
-      for (const yOffset of this.offsets.y) {
-        const coordsB = {
-          x: coordsA.x + xOffset,
-          y: coordsA.y + yOffset,
-        }
-        const deltas = coordsA.deltas(coordsB)
-        if (this.validations.exceptions(deltas)) {
-          adjacentCoords.push(new Coords(coordsB))
+    this.constraints.forEach(constraint => {
+      const offsets = this.buildRangeSet(constraint)
+
+      for (const xOffset of offsets.x) {
+        for (const yOffset of offsets.y) {
+          const coordsB = {
+            x: coordsA.x + xOffset,
+            y: coordsA.y + yOffset,
+          }
+          const deltas = coordsA.deltas(coordsB)
+          if (
+            !constraint.exceptions ||
+            constraint.exceptions.every(exception => exception(deltas))
+          ) {
+            adjacentCoords.push(new Coords(coordsB))
+          }
         }
       }
-    }
+    })
     return adjacentCoords
   }
-  /**
-   * validations to run on deltas
-   */
-  private validations = {
-    offsets: (deltas: Coords) =>
-      this.offsets.x.has(deltas.x) && this.offsets.y.has(deltas.y),
-    exceptions: (deltas: Coords) =>
-      this.constraint.exceptions?.every(e => e(deltas)) ?? true,
-  }
 
-  private buildRangeSets() {
-    return (['x', 'y'] as const).reduce(
+  private buildRangeSet = ({ offsets }: RangeConstraintConfig) =>
+    (['x', 'y'] as const).reduce(
       (acc, key) => {
-        this.constraint.offsets[key].forEach(offset => {
+        offsets[key].forEach(offset => {
           const offsetRange = Array.isArray(offset)
             ? range(offset[0], offset[1] + 1)
             : [offset]
@@ -65,5 +51,4 @@ export default class RangeConstraint {
       },
       { x: new Set<number>(), y: new Set<number>() }
     )
-  }
 }
