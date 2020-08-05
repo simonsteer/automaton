144ad2f25b164268e140b16c4af9527175bc5707
drop-simon <simon@earnwithdrop.com>
2020-08-05 18:27:25 -0400

    ting

--- src/entities/Terrain/Terrain.ts
+++ src/entities/Terrain/Terrain.ts
@@ -3,7 +3,6 @@ import { TerrainConfig } from './types'
 export default class Terrain {
   readonly id = Symbol()
   cost = (unit: Unit) => 1
-  cache = new Map<Symbol, number>()
 
   constructor({ cost } = {} as TerrainConfig) {
     if (cost) this.cost = cost
